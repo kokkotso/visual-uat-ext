eafec65652e819091c3a3819e30142384aa6f890
Kokko Tso <kokkotso@gmail.com>
2021-02-24 21:30:19 -0500

    Adjust CSS in popup

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 	const [url, setUrl] = useState("");
 	const [width, setWidth] = useState(0);
 	const [screenshot, setScreenshot] = useState("");
+	const [bugProblem, setBugProblem] = useState("");
 
 	function captureScreen() {
 		chrome.tabs.captureVisibleTab((dataUrl) => {
@@ -41,8 +42,24 @@ function App() {
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>The current URL is: {url}</p>
 				<p>The window is {width}px wide</p>
+				<h2>Report a Bug</h2>
+				<form>
+					<label for="bugProblem">Describe problem:</label>
+					<input
+						id="bugProblem"
+						type="text"
+						value={bugProblem}
+						onChange={(ev) => setBugProblem(ev.target.value)}
+					></input>
+				</form>
+
 				<button onClick={captureScreen}>Take Screenshot</button>
-				{screenshot && <img alt="screenshot" src={screenshot} />}
+				{screenshot && (
+					<div>
+						<h2>Preview</h2>
+						<img alt="screenshot" src={screenshot} />
+					</div>
+				)}
 			</header>
 		</div>
 	);
